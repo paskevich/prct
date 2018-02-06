7d23ec8a1ea9242e54056766a73a982ea8848dcd
Artem <konuchovartem@gmail.com>
2018-02-06 13:25:52 +0300

    add login activity

--- app/src/main/java/com/example/paskevich/imprecity/MainActivity.java
+++ app/src/main/java/com/example/paskevich/imprecity/MainActivity.java
@@ -4,15 +4,32 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.example.paskevich.imprecity.utils.AuthorizationUtils;
+
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //clause
+
+        if (!AuthorizationUtils.isAuthorized(this)) {
+            onLogout();
+            return;
+        }
+
         Intent intent = new Intent(this, TabbedActivity.class);
         startActivity(intent);
         //exit or smth else
     }
+
+    private void onLogout()
+    {
+        Intent login = new Intent(this, LoginActivity.class);
+        login.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        startActivity(login);
+        finish();
+    }
+
 }
---
+++ app/src/main/java/com/example/paskevich/imprecity/utils/AuthorizationUtils.java
@@ -0,0 +1,53 @@
+package com.example.paskevich.imprecity.utils;
+
+/**
+ * Created by Artem on 06.02.2018.
+ */
+
+import android.content.Context;
+
+public class AuthorizationUtils {
+    private static final String PREFERENCES_AUTHORIZED_KEY = "isAuthorized";
+    private static final String LOGIN_PREFERENCES = "LoginData";
+
+    /**
+     * This method makes the user authorized
+     *
+     * @param context current context
+     */
+
+    public static void setAuthorized(Context context)
+    {
+        context.getSharedPreferences(LOGIN_PREFERENCES, Context.MODE_PRIVATE)
+                .edit()
+                .putBoolean(PREFERENCES_AUTHORIZED_KEY, true)
+                .apply();
+    }
+
+    /**
+     * This method makes the user unauthorized
+     *
+     * @param context current context
+     */
+
+    public static void logout(Context context)
+    {
+        context.getSharedPreferences(LOGIN_PREFERENCES, Context.MODE_PRIVATE)
+                .edit()
+                .putBoolean(PREFERENCES_AUTHORIZED_KEY, false)
+                .apply();
+    }
+
+    /**
+     * This method checks if the user is authorized
+     *
+     * @param context current context
+     * @return {@code true} if the user is authorized and {@code false} if not
+     */
+
+    public static boolean isAuthorized(Context context)
+    {
+        return context.getSharedPreferences(LOGIN_PREFERENCES, Context.MODE_PRIVATE)
+                .getBoolean(PREFERENCES_AUTHORIZED_KEY, false);
+    }
+}
